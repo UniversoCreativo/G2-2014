99a2c70cd372b0620c82f73d727578d938a42fe3
Asurax <cato_el17@hotmail.com>
2014-08-14 18:36:18 -0300

    actualizando archivos

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 <html>
 <head>
 <meta charset="utf-8">
+<link href="css/login.css" rel="stylesheet" type="text/css" />
 <title>Documento sin título</title>
 </head>
 
 <body>
+<div id="login">
+ 		<div id="formulario"><strong>Usuraio </strong>
+ 		  <label for="textfield">:</label>
+          <input type="text" name="textfield" id="textfield">
+          <p><strong>Contraseña</strong>: <input type="password"> </p>
+ 		</div>
+</div>
+
+
 </body>
 </html>
