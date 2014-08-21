36e49786efe6c2b65842bdfca14cf62f0e611f95
Asurax <cato_el17@hotmail.com>
2014-08-21 19:16:14 -0300

    Modificacion 21/08/2014
    hicimos funcionar el  formulario de ingreso!!

--- index.php
+++ index.php
@@ -4,21 +4,45 @@
 <meta charset="utf-8">
 <title>Documento sin título</title>
 <link rel="StyleSheet" href="style.css" type="text/css">
+<script type="text/javascript" src="js/jquery-2.1.1.min (1).js"></script>
 </head>
 
 <body>
 <div id="login">
 <div id="formulario">
-<form id="form1" name="form1" action="acceso.php" method="POST">
+<form id="f_acceso" name="f_acceso" action="" method="POST"  onSubmit="return validarFormulario();"> 
   <div id="nombre">
-      <input type="text" name="f_usuario" id="f_usuario" placeholder="Usuario"  size="30">
+      <input type="text" name="f_usuario" id="f_usuario">  
    </div>
    <div id="contraseña">
-      <input type="text" name="f_contra" id="f_contra" placeholder="Contraceña" size="30">
+      <input type="password" name="f_contra" id="f_contra">
    </div>
    <div id="enviar">
-     <input type="submit" name="button" id="button" value="Enviar">
+     <input type="submit" name="f_boton" id="f_boton" value="Enviar">
    </div>
    <div> <a href="#" class="a">Ha olvidado su contraseña</a></div></form></div></div>
+   <div id="mensajeError" style="color:#F00;font-size:12px;"></div>
+   
+   <script>
+   
+   $("#f_usuario").focus();
+   function validarFormulario(){
+	   var f_usuario = $("#f_usuario").val();
+	   var f_contra = $("#f_contra").val();
+	   
+	   var f_usuario_cantidad = f_usuario.length;
+	   var f_contra_cantidad = f_contra.lenght;  
+	   
+	if (f_usuario == ""){
+		$("#mensajeError").html("<strong>ALERTA</strong> Usted no ingreso el usuario");
+		return false;
+        }
+   if (f_contra == "") {
+	   $("#mensajeError").html("<strong>ALERTA</strong> Usted no ingreso la contraseña");
+	   return false;
+   }
+   return true;
+   }
+   </script>
 </body>
 </html>
